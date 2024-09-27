9ed6aace920750438374041f4938dc6a21e4f749
yongseockcho <yongseock.cho@nstage.io>
2024-09-27 17:31:59 +0900

    - fix: init.sql 테이블 변경, Readme.md "완성 예정 기간" 기재

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 SET NAMES utf8mb4;
 
 CREATE DATABASE IF NOT EXISTS `users`;
-CREATE DATABASE IF NOT EXISTS `payments`;
+CREATE DATABASE IF NOT EXISTS `products`;
 
 
 CREATE USER 'users'@'%' IDENTIFIED BY 'dev00_users';
@@ -10,11 +10,11 @@ CREATE USER 'users_slave'@'%' IDENTIFIED BY 'dev00_users';
 GRANT SELECT,UPDATE,DELETE,INSERT ON `users`.* TO 'users'@'%';
 GRANT SELECT ON `users`.* TO 'users_slave'@'%';
 
-CREATE USER 'payments'@'%' IDENTIFIED BY 'dev00_payments';
-CREATE USER 'payments_slave'@'%' IDENTIFIED BY 'dev00_payments';
+CREATE USER 'products'@'%' IDENTIFIED BY 'dev00_products';
+CREATE USER 'products_slave'@'%' IDENTIFIED BY 'dev00_products';
 
-GRANT SELECT,UPDATE,DELETE,INSERT ON `payments`.* TO 'payments'@'%';
-GRANT SELECT ON `payments`.* TO 'payments_slave'@'%';
+GRANT SELECT,UPDATE,DELETE,INSERT ON `products`.* TO 'products'@'%';
+GRANT SELECT ON `products`.* TO 'products_slave'@'%';
 
 FLUSH PRIVILEGES;
 
