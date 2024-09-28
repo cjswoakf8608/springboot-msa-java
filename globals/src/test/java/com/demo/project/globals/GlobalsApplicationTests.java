8688753e7bb4bdbb972c110b85aa945ffad74698
yongseockcho <yongseock.cho@nstage.io>
2024-09-28 15:09:33 +0900

    feat: 전체 패키지 구조 및 globals 공통 소스 작성

---
+++ globals/src/test/java/com/demo/project/globals/GlobalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.project.globals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
