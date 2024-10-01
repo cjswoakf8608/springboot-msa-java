2475b0b5e68e7c756f8d92548e2bb8f14450dd45
yongseockcho <yongseock.cho@nstage.io>
2024-10-01 21:57:16 +0900

    fix: docker-compose.yml 수정
    Workers 도메인 추가, redis 추가

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@ cd "$(dirname "$0")"
 # 각 모듈 빌드 실행
 (cd apis/apps && ./gradlew clean bootJar --no-daemon)
 (cd apis/payments && ./gradlew clean bootJar --no-daemon)
-(cd apis/auths && ./gradlew clean bootJar --no-daemon)
 (cd cores/core-products && ./gradlew clean bootJar --no-daemon)
 (cd cores/core-users && ./gradlew clean bootJar --no-daemon)
+(cd wordkers && ./gradlew clean bootJar --no-daemon)
 
 # docker-compose 실행
 docker-compose up --build
